3f1d5725e102009791377f8db0ea69724d48be74
Keworker <79995640807la@gmail.com>
2023-09-09 21:05:55 +0500

    Add title to main window (icon + text).

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,20 +1,19 @@
-from PyQt6.QtWidgets import QWidget, QPushButton
-from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QLayout, QScrollArea, QWidget
+from PyQt6.QtGui import QIcon, QPixmap, QFont
 from typing import NoReturn as Unit
 
 
-class MainWindow(QWidget):  # {
-    def __init__(self, title: str, iconPath: str):  # {
+class MainWindow(QMainWindow):  # {
+    def __init__(self, title: str, iconSmallPath: str, iconLargePath: str):  # {
         super().__init__()
-        self.setWindowTitle(title)
-        self.setWindowIcon(QIcon(iconPath))
-        self.__initWidgets()
+        self.__initWidgets(title, iconSmallPath, iconLargePath)
     # }
 
-    def __initWidgets(self) -> Unit:  # {
+    def __initWidgets(self, title: str, iconSmallPath: str, iconLargePath: str) -> Unit:  # {
         # TODO:
         #  We should init our widgets here. They are:
-        #  1. Title
+        #  1. DONE: Title
         #  2. Radio group file mask / regex
         #  3. Edit text for files search rules
         #  4. Checkbox search in body
@@ -23,6 +22,35 @@ class MainWindow(QWidget):  # {
         #  7. Log in with GitHub button - just addition feature
         #  8. Repository for searching (author/repo-name) - just addition feature
         #  9. List with results
-        pass
+        self.scroll = QScrollArea()
+        self.widget = QWidget()
+        root: QVBoxLayout = QVBoxLayout()
+        root.setAlignment(Qt.AlignmentFlag.AlignTop)
+        root.addLayout(self.__getTitleLayout(title, iconLargePath))
+        self.widget.setLayout(root)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(1)
+        self.scroll.setWidget(self.widget)
+        self.setCentralWidget(self.scroll)
+        self.setWindowTitle(title)
+        self.setWindowIcon(QIcon(iconSmallPath))
+        self.showMaximized()
+    # }
+
+    @staticmethod
+    def __getTitleLayout(title: str, iconLargePath: str) -> QLayout:  # {
+        titleContainer: QHBoxLayout = QHBoxLayout()
+        title: QLabel = QLabel(title)
+        font: QFont = title.font()
+        font.setBold(1)
+        title.setFont(font)
+        title.setMaximumHeight(title.fontMetrics().height() * 4)
+        icon: QLabel = QLabel()
+        icon.setPixmap(QPixmap(iconLargePath).scaledToHeight(title.height()))
+        titleContainer.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        titleContainer.addWidget(icon)
+        titleContainer.addWidget(title)
+        return titleContainer
     # }
 # }
