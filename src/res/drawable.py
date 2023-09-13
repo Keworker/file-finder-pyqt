fd6e9a32b676f80b03e69961ad3e7da2f3960817
Keworker <79995640807la@gmail.com>
2023-09-13 19:50:41 +0500

    Move assets to resources. Sort requirements by alphabet.

--- src/res/drawable.py
+++ src/res/drawable.py
@@ -2,6 +2,6 @@ from pathlib import Path
 import os as OS
 
 
-__ASSETS_DIR: str = OS.path.join(str(Path(__file__).parent.parent.parent), "assets")
+__ASSETS_DIR: str = OS.path.join(str(Path(__file__).parent), "assets")
 APP_ICON_256: str = OS.path.join(__ASSETS_DIR, "icon256.ico")
 APP_ICON_1024: str = OS.path.join(__ASSETS_DIR, "icon1024.png")
