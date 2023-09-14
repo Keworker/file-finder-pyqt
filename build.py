64bcbfa67a99d6b7f53e24036b8133286c2c828d
Keworker <79995640807la@gmail.com>
2023-09-14 19:34:41 +0500

    Change package structure. Init build script.

---
+++ build.py
@@ -0,0 +1,15 @@
+import PyInstaller.__main__
+import os as OS
+
+from src.res.drawable import ASSETS_DIR, APP_ICON_256, ASSETS_PLACEHOLDER
+from src.res.strings import APP_TITLE
+
+# TODO: Fix problem with assets in .exe build.
+PyInstaller.__main__.run(
+    [
+        OS.path.join("src", "python", "application.py"),
+        "--windowed",
+        "-n", APP_TITLE,
+        "-i", APP_ICON_256.replace(ASSETS_PLACEHOLDER + ':', ASSETS_DIR)
+    ]
+)
