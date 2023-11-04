36bc5a4b72c9a7808e537c540da14437e6c83f57
Keworker <79995640807la@gmail.com>
2023-11-04 17:39:41 +0500

    Revert "Revert "Commit for collective work at 'Resources are broken at Windows builds. #4'.""
    This reverts commit 7248fab40fcea289fee2aaa4a92843e89adfb695.

--- build.py
+++ build.py
@@ -1,14 +1,9 @@
-import os as OS
-import PyInstaller.__main__
+import subprocess as Subprocess
+import platform as Platform
 
-from src.res.drawable import ASSETS_DIR, APP_ICON_256, ASSETS_PLACEHOLDER
-from src.res.strings import APP_TITLE
-
-PyInstaller.__main__.run(
-    [
-        OS.path.join("src", "python", "application.py"),
-        "--windowed",
-        "-n", APP_TITLE,
-        "-i", APP_ICON_256.replace(ASSETS_PLACEHOLDER + ':', ASSETS_DIR)
-    ]
-)
+if (Platform.system() == "Windows"):  # {
+    Subprocess.run("pyinstaller Finder-chan-windows.spec")
+# }
+else:  # {
+    pass
+# }
